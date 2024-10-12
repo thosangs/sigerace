95e7eb91b007b03122d81685a55f25e403b1b4c6
Seceng <thosan.girisona@gmail.com>
2024-10-12 12:31:46 +0700

    feat: change jadwal

--- lib/jadwal.ts
+++ lib/jadwal.ts
@@ -2,41 +2,46 @@ const listJadwal = [
 	{
 		details: "REGISTRASI",
 		time: "07:00 - 11:30",
-		note: "-",
+		note: "Jadwal Berubah Menyesuaikan Hujan",
 	},
 	{
 		details: "PEMBUKAAN & TEST TRACK",
-		time: "07:00 - 08:00",
+		time: "07:00 - 10:00",
 		note: "-",
 	},
 	{
 		details: "2022 MIX",
-		time: "08:00 - 09:00",
-		note: "-",
+		time: "10:00 - 11:00",
+		note: "Jadwal Berubah Menyesuaikan Hujan",
 	},
 	{
 		details: "2021 GIRL & BOY",
-		time: "09:00 - 10:00",
-		note: "-",
+		time: "11:00 - 12:00",
+		note: "Jadwal Berubah Menyesuaikan Hujan",
 	},
 	{
 		details: "2020 GIRL & BOY",
-		time: "10:00 - 10:30",
+		time: "12:00 - 12:30",
+		note: "Jadwal Berubah Menyesuaikan Hujan",
+	},
+	{
+		details: "ISHOMA",
+		time: "12:30 - 13:00",
 		note: "-",
 	},
 	{
 		details: "2019/18 GIRL - 2019 BOY",
-		time: "10:30 - 11:00",
+		time: "13:00 - 13:30",
 		note: "-",
 	},
 	{
 		details: "2018 & 2017 BOY",
-		time: "11:00 - 11:30",
+		time: "13:30 - 14:00",
 		note: "-",
 	},
 	{
 		details: "FFA GIRL & BOY",
-		time: "11:30 - 13:00",
+		time: "14:00 - 14:30",
 		note: "-",
 	},
 ];
