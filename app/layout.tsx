ba037d5db2ab5dd63cc038866475719222f9644a
Seceng <thosan.girisona@gmail.com>
2025-04-21 06:59:51 +0700

    change color bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <div className="flex flex-col min-h-screen bg-gradient-to-b from-primary dark:to-primary-foreground to-primary">
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <div className="flex flex-col min-h-screen bg-background dark:bg-background [background:radial-gradient(circle_at_top_left,hsl(var(--background))_0%,hsl(var(--primary)/_0.3)_25%,hsl(var(--accent)/_0.3)_50%,hsl(var(--primary)/_0.3)_75%,hsl(var(--background))_100%)]">
             <Header />
 
             <main className="flex-1 flex flex-col items-center p-2">
