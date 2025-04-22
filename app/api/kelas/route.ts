79ff34d85781cc5e4c2c94dadbac42e9a28c7e90
Seceng <thosan.girisona@gmail.com>
2025-04-22 10:47:47 +0700

    debug mode

--- app/api/kelas/route.ts
+++ app/api/kelas/route.ts
@@ -87,6 +87,9 @@ export async function GET(req: NextRequest) {
 
     return NextResponse.json(data, { status: 200 });
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json(
+      { error: error.message + apiUrl },
+      { status: 500 }
+    );
   }
 }
