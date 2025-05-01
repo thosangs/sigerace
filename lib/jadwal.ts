895b68e238f64035d4ca855a1cdfeef84ab3620b
Seceng <thosan.girisona@gmail.com>
2025-05-01 16:36:43 +0700

    Change jadwal

--- lib/jadwal.ts
+++ lib/jadwal.ts
@@ -1,49 +1,84 @@
 const listJadwal = [
-	{
-		details: "REGISTRASI",
-		time: "07:00 - 11:30",
-		note: "Jadwal Berubah Menyesuaikan Hujan",
-	},
-	{
-		details: "PEMBUKAAN & TEST TRACK",
-		time: "07:00 - 10:00",
-		note: "-",
-	},
-	{
-		details: "2022 MIX",
-		time: "10:00 - 11:00",
-		note: "Jadwal Berubah Menyesuaikan Hujan",
-	},
-	{
-		details: "2021 GIRL & BOY",
-		time: "11:00 - 12:00",
-		note: "Jadwal Berubah Menyesuaikan Hujan",
-	},
-	{
-		details: "2020 GIRL & BOY",
-		time: "12:00 - 12:30",
-		note: "Jadwal Berubah Menyesuaikan Hujan",
-	},
-	{
-		details: "ISHOMA",
-		time: "12:30 - 13:00",
-		note: "-",
-	},
-	{
-		details: "2019/18 GIRL - 2019 BOY",
-		time: "13:00 - 13:30",
-		note: "-",
-	},
-	{
-		details: "2018 & 2017 BOY",
-		time: "13:30 - 14:00",
-		note: "-",
-	},
-	{
-		details: "FFA GIRL & BOY",
-		time: "14:00 - 14:30",
-		note: "-",
-	},
+  {
+    details: "REGISTRASI",
+    time: "07:00 - 15:00",
+    note: "-",
+  },
+  {
+    details: "PEMBUKAAN TEST TRACK",
+    time: "07:00 - 08:00",
+    note: "-",
+  },
+  {
+    details: "2023 MIX",
+    time: "08:00 - 08:12",
+    note: "-",
+  },
+  {
+    details: "2022 BOYS",
+    time: "08:12 - 09:22",
+    note: "-",
+  },
+  {
+    details: "2022 GIRLS",
+    time: "09:22 - 09:46",
+    note: "-",
+  },
+  {
+    details: "2018 BOYS",
+    time: "09:46 - 10:10",
+    note: "-",
+  },
+  {
+    details: "2018 GIRLS",
+    time: "09:46 - 10:10",
+    note: "-",
+  },
+  {
+    details: "2019 BOYS",
+    time: "10:10 - 11:20",
+    note: "-",
+  },
+  {
+    details: "2019 GIRLS",
+    time: "11:20 - 11:44",
+    note: "-",
+  },
+  {
+    details: "ISOMA",
+    time: "12:00 - 13:00",
+    note: "-",
+  },
+  {
+    details: "2021 BOYS",
+    time: "13:00 - 15:30",
+    note: "-",
+  },
+  {
+    details: "2021 GIRLS",
+    time: "15:30 - 15:42",
+    note: "-",
+  },
+  {
+    details: "2020 BOYS",
+    time: "15:42 - 16:42",
+    note: "-",
+  },
+  {
+    details: "2020 GIRLS",
+    time: "16:42 - 17:06",
+    note: "-",
+  },
+  {
+    details: "FFA MAX 2015 BOYS",
+    time: "17:06 - 17:36",
+    note: "-",
+  },
+  {
+    details: "FFA MAX 2015 GIRLS",
+    time: "17:06 - 17:36",
+    note: "-",
+  },
 ];
 
 export default listJadwal;
